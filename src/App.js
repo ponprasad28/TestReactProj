12440d64c2cc2475fa4aa5a59bf52f23578f1007
ponprasad28 <70145028+ponprasad28@users.noreply.github.com>
2023-07-05 18:03:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Pon Prasad.
+          Edit <code>src/App.js</code> and Welcome.
         </p>
         <a
           className="App-link"
